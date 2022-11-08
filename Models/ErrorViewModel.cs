920132d69c7514f6ae89843995fc436ec7f43e59
drago333 <60980007+drago333@users.noreply.github.com>
2022-11-08 14:57:38 +0200

    Add project files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DotnetMvcWithAuthTemplate.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
